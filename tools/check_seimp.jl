1b9b93bd1ecd0037d9195090e705bcc3c018d6d4
jm@anderson10.20.5.8 <jxsrwjm1996@gmail.com>
2023-07-29 15:38:42 +0800

    up date tools

--- tools/check_seimp.jl
+++ tools/check_seimp.jl
@@ -1,18 +1,21 @@
-
 using DelimitedFiles
 
-# println("The out put is in: "*pwd()*"/out")# for customers.
+norb = 3
 
-# mkdir("out")
-norb = 2
-b = readdir()
-for a in b
-    if (a != "code") 
-        m = readdlm(a)
-        println("test the ",a)
-        for j=(norb*norb)+3:(norb*norb)*2+2
-            for i=2:10
-                if(m[i, j]>0) println(a,"  ",m[i, 27],"  ",m[1, j],"  ",m[i, j])  end
+# 获取当前目录下的所有文件名
+all_files = readdir()
+
+# 筛选出所有以 "seimp.txt." 开头的文件
+target_files = filter(filename -> startswith(filename, "seimp.txt"), all_files)
+
+# 对筛选出的文件进行操作
+for filename in target_files
+    m = readdlm(filename)
+    println("Processing file: ", filename)
+    for j = (norb*norb) + 3 : (norb*norb) * 2 + 2
+        for i = 2 : 10
+            if m[i, j] > 0 
+                println(filename, "  ", m[i, (norb*norb) + 2], "  ", m[1, j], "  ", m[i, j])  
             end
         end
     end
